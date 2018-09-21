c88a4ef551075f35e5f049267e79d4e432b173bd
raghuravikumar <43379222+raghuravikumar@users.noreply.github.com>
2018-09-21 12:42:38 +0530

    Update Application.java

--- org-management/src/main/java/com/cisco/cjp/org/management/Application.java
+++ org-management/src/main/java/com/cisco/cjp/org/management/Application.java
@@ -12,5 +12,9 @@ public class Application {
 	public static void main(final String[] args) {
 		SpringApplication.run(Application.class, args);
 		LOG.info("ORG-MANAGEMENT: Application Up & running");
+		
+		if(){}
+		if(){}
+			
 	}
 }
